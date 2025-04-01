a309b083565f212dd7873295cada7f51e5402fb6
Elmohaseb-Lsiyaha <angel.science0@gmail.com>
2025-04-01 07:59:50 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1 class="text-center my-5">مرحبا بكم في عيادات الحجايزة</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+@import url('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css');
+body {
+  direction: rtl;
+  font-family: 'Tajawal', sans-serif;
+}
+</style>
